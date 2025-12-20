7b76514be0d7cb4bb20ec3cfcb8a6874c7094ab2
eirala <eirala@teceng-gaming.com.ar>
2025-12-20 19:29:38 -0300

    avances

--- fluxara/internal/adapters/rest/ping.go
+++ fluxara/internal/adapters/rest/ping.go
@@ -1,9 +1,18 @@
 package rest
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 func (h *Handlers) Ping() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 
+		fmt.Printf("Esta bien ping\n")
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		json.NewEncoder(w).Encode(http.StatusOK)
 	}
 }
